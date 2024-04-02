93c6cdf1c09291f7b00b5c911940df064ac84fd8
AcmenYXH <2458892691@qq.com>
2024-04-02 22:56:59 +0800

    @RabbitMQ学习@mq学习demo

---
+++ mq-demo/publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
@@ -0,0 +1,61 @@
+package com.itheima.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+public class SpringAmqpTest {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    void testSendMessage2Queue() {
+        String queueName = "simple.queue";
+        String msg = "hello, amqp!";
+        rabbitTemplate.convertAndSend(queueName, msg);
+    }
+
+    @Test
+    void testWorkQueue() throws InterruptedException {
+        String queueName = "work.queue";
+        for (int i = 1; i <= 50; i++) {
+            String msg = "hello, worker, message_" + i;
+            rabbitTemplate.convertAndSend(queueName, msg);
+            Thread.sleep(20);
+        }
+    }
+
+    @Test
+    void testSendFanout() {
+        String exchangeName = "hmall.fanout";
+        String msg = "hello, everyone!";
+        rabbitTemplate.convertAndSend(exchangeName, null, msg);
+    }
+
+    @Test
+    void testSendDirect() {
+        String exchangeName = "hmall.direct";
+        String msg = "蓝色通知，警报解除，哥斯拉是放的气球";
+        rabbitTemplate.convertAndSend(exchangeName, "blue", msg);
+    }
+
+    @Test
+    void testSendTopic() {
+        String exchangeName = "hmall.topic";
+        String msg = "今天天气挺不错，我的心情的挺好的";
+        rabbitTemplate.convertAndSend(exchangeName, "china.weather", msg);
+    }
+
+    @Test
+    void testSendObject() {
+        Map<String, Object> msg = new HashMap<>(2);
+        msg.put("name", "jack");
+        msg.put("age", 21);
+        rabbitTemplate.convertAndSend("object.queue", msg);
+    }
+}
