88b8a5fa7be81fa78b5d10dbe28caa9b66ba5687
AcmenYXH <2458892691@qq.com>
2024-05-27 21:29:13 +0800

    @java版本的新特性@JDK8的新特性@Lambda表达式

---
+++ new-features-for-java/src/main/java/com/yangxh/Main.java
@@ -0,0 +1,12 @@
+package com.yangxh;
+
+/**
+ * @Description Java版本的新特性
+ * @Author yangxh8
+ * @Date 2024/5/27 21:24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ new-features-for-java/src/main/java/com/yangxh/jdk8/Lambda.java
@@ -0,0 +1,9 @@
+package com.yangxh.jdk8;
+
+/**
+ * @Description JDK8新特性--Lambda表达式
+ * @Author yangxh8
+ * @Date 2024/5/27 21:26
+ */
+public class Lambda {
+}
