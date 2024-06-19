35af9b0176ce8f15fc75a86c1d11054d058f6e3d
AcmenYXH <2458892691@qq.com>
2024-06-19 22:11:35 +0800

    @2024SpringCloud教程@支付模块

---
+++ cloud2024/cloud-provider-payment8001/src/main/java/com/yangxh/cloud/Main.java
@@ -0,0 +1,12 @@
+package com.yangxh.cloud;
+
+/**
+ * @Description ${description}
+ * @Author yangxh8
+ * @Date 2024/6/19 22:08
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
