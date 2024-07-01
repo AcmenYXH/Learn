2fc8b3b4c775012e770da39bd9839b0e9d0cd910
AcmenYXH <2458892691@qq.com>
2024-07-01 22:35:30 +0800

    @2024SpringCloud教程@支付模块@统一返回值

--- cloud2024/cloud-provider-payment8001/src/main/java/com/yangxh/cloud/controller/PayController.java
+++ cloud2024/cloud-provider-payment8001/src/main/java/com/yangxh/cloud/controller/PayController.java
@@ -3,6 +3,7 @@ package com.yangxh.cloud.controller;
 import com.alibaba.fastjson2.JSON;
 import com.yangxh.cloud.entities.Pay;
 import com.yangxh.cloud.entities.PayDTO;
+import com.yangxh.cloud.resp.ResultData;
 import com.yangxh.cloud.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -29,50 +30,56 @@ public class PayController {
 
     @PostMapping(value = "/add")
     @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
-    public String addPay(@RequestBody Pay pay) throws Exception {
+    public ResultData<String> addPay(@RequestBody Pay pay) throws Exception {
         if (log.isDebugEnabled()) {
             log.debug("PayController--addPay--pay={}", JSON.toJSONString(pay));
         }
         int count = payService.add(pay);
-        return "成功插入记录，返回值：" + count;
+//        return "成功插入记录，返回值：" + count;
+        return ResultData.success("成功插入记录，返回值：" + count);
     }
     
     @DeleteMapping(value = "/delete/{id}")
     @Operation(summary = "删除",description = "删除支付流水方法")
-    public String deletePay(@PathVariable("id") Integer id) throws Exception {
+    public ResultData<String> deletePay(@PathVariable("id") Integer id) throws Exception {
         if (log.isDebugEnabled()) {
             log.debug("PayController--deletePay--id={}", JSON.toJSONString(id));
         }
         int count = payService.delete(id);
-        return "成功删除记录，返回值：" + count;
+//        return "成功删除记录，返回值：" + count;
+        return ResultData.success("成功删除记录，返回值：" + count);
+
     }
 
     @PutMapping(value = "/update")
     @Operation(summary = "更新", description = "修改支付流水方法")
-    public String updatePay(@RequestBody PayDTO payDTO) throws Exception {
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO) throws Exception {
         if (log.isDebugEnabled()) {
             log.debug("PayController--updatePay--pay={}", JSON.toJSONString(payDTO));
         }
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
         int count = payService.update(pay);
-        return "成功更新记录，返回值：" + count;
+//        return "成功更新记录，返回值：" + count;
+        return ResultData.success("成功更新记录，返回值：" + count);
     }
 
     @GetMapping(value = "/get/{id}")
     @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
-    public String getPayById(@PathVariable("id") Integer id) throws Exception {
+    public ResultData<Pay> getPayById(@PathVariable("id") Integer id) throws Exception {
         if (log.isDebugEnabled()) {
             log.debug("PayController--getPayById--id={}", JSON.toJSONString(id));
         }
         Pay pay = payService.getById(id);
-        return "成功查询记录，返回值：" + pay;
+//        return "成功查询记录，返回值：" + pay;
+        return ResultData.success(pay);
+
     }
 
     @GetMapping(value = "/getAll")
     @Operation(summary = "查询全部流水",description = "查询全部支付流水方法")
-    public String getAllPay() throws Exception {
+    public ResultData<List<Pay>> getAllPay() throws Exception {
         List<Pay> all = payService.getAll();
-        return "查询所有记录，返回值：" + JSON.toJSONString(all);
+        return ResultData.success(all);
     }
 }
