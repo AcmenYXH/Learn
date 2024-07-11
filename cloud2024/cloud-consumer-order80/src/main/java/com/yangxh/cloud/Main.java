10540bad74cdee2201ae90ebccad4cfc12e5b687
AcmenYXH <2458892691@qq.com>
2024-07-11 22:02:19 +0800

    @2024SpringCloud教程@订单模块

---
+++ cloud2024/cloud-consumer-order80/src/main/java/com/yangxh/cloud/Main.java
@@ -0,0 +1,12 @@
+package com.yangxh.cloud;
+
+/**
+ * @Description ${description}
+ * @Author yangxh8
+ * @Date 2024/7/11 21:59
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
