d1bac4f1819f9b32795d8619f5f05709988a9257
AcmenYXH <2458892691@qq.com>
2024-07-14 22:23:13 +0800

    @2024SpringCloud教程@通用模块

---
+++ cloud2024/cloud-api-commons/src/main/java/com/yangxh/cloud/Main.java
@@ -0,0 +1,12 @@
+package com.yangxh.cloud;
+
+/**
+ * @Description ${description}
+ * @Author yangxh8
+ * @Date 2024/7/14 17:23
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
