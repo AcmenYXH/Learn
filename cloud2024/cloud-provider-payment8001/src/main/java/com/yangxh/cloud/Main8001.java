37f2af085c503024ece91d86ae30c7d1fda50216
AcmenYXH <2458892691@qq.com>
2024-07-21 21:09:52 +0800

    @2024SpringCloud教程@宿舍电脑本地配置@Consul的动态刷新

--- cloud2024/cloud-provider-payment8001/src/main/java/com/yangxh/cloud/Main8001.java
+++ cloud2024/cloud-provider-payment8001/src/main/java/com/yangxh/cloud/Main8001.java
@@ -3,6 +3,7 @@ package com.yangxh.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -13,6 +14,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan("com.yangxh.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
 @EnableDiscoveryClient //该注解用于向使用consul为注册中心时注册服务
+@RefreshScope // Consul的动态刷新配置
 public class Main8001 {
     public static void main(String[] args) {
         SpringApplication.run(Main8001.class, args);
