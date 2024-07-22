2e45cbb44e2c82fe8694ddc6cdef5b1e69455e59
AcmenYXH <2458892691@qq.com>
2024-07-22 22:29:21 +0800

    @2024SpringCloud教程@服务调用负载均衡@LoadBalancer服务调用负载均衡示例

--- cloud2024/cloud-consumer-order80/src/main/java/com/yangxh/cloud/controller/OrderController.java
+++ cloud2024/cloud-consumer-order80/src/main/java/com/yangxh/cloud/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.yangxh.cloud.controller;
 
 import com.yangxh.cloud.entities.PayDTO;
 import com.yangxh.cloud.resp.ResultData;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @Description
@@ -54,5 +59,25 @@ public class OrderController {
     @GetMapping(value = "/pay/get/info")
     private ResultData getInfoByConsul() {
         return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/info", ResultData.class);
+    }
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/discovery")
+    public String discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            System.out.println(element);
+        }
+
+        System.out.println("===================================");
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance element : instances) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t" + element.getUri());
+        }
+
+        return instances.get(0).getServiceId() + ":" + instances.get(0).getPort();
     }
 }
