265b264a0ab9d1992e9a67d824151c9db8f639be
AcmenYXH <2458892691@qq.com>
2024-07-22 22:48:59 +0800

    @2024SpringCloud教程@服务调用负载均衡@LoadBalancer算法切换为随机

--- cloud2024/cloud-consumer-order80/src/main/java/com/yangxh/cloud/config/RestTemplateConfig.java
+++ cloud2024/cloud-consumer-order80/src/main/java/com/yangxh/cloud/config/RestTemplateConfig.java
@@ -1,8 +1,15 @@
 package com.yangxh.cloud.config;
 
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -11,11 +18,21 @@ import org.springframework.web.client.RestTemplate;
  * @Date 2024/7/11 22:42
  */
 @Configuration
+@LoadBalancerClient(
+        //下面的value值大小写一定要和consul里面的名字一样，必须一样
+        value = "cloud-payment-service", configuration = RestTemplateConfig.class)
 public class RestTemplateConfig {
-
     @Bean
-    @LoadBalanced
+    @LoadBalanced //使用@LoadBalanced注解赋予RestTemplate负载均衡的能力
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(Environment environment,
+                                                            LoadBalancerClientFactory loadBalancerClientFactory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+
+        return new RandomLoadBalancer(loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class), name);
     }
 }
