6559f8fd104d99a5f1ba75dc901795c3d0a9be76
AcmenYXH <2458892691@qq.com>
2024-07-24 22:56:17 +0800

    @2024SpringCloud教程@服务调用负载均衡@OpenFeign开启请求/响应压缩

--- cloud2024/cloud-consumer-fegin-order80/src/main/java/com/yangxh/cloud/config/FeginConfig.java
+++ cloud2024/cloud-consumer-fegin-order80/src/main/java/com/yangxh/cloud/config/FeginConfig.java
@@ -1,5 +1,6 @@
 package com.yangxh.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +16,8 @@ public class FeginConfig {
     @Bean
     public Retryer myRetryer() {
         // 默认重试机制是不开启的
-        return Retryer.NEVER_RETRY;
+//        return Retryer.NEVER_RETRY;
         // 最大请求次数为3(1(Default)+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
-//        return new Retryer.Default(100, 1, 3);
+        return new Retryer.Default(100, 1, 3);
     }
 }
